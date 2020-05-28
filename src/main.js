80d4340d033e1f3f040495272c062cda9a6818f5
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-28 11:33:00 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,56 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
+import Paginate from "vuejs-paginate";
+import VueMeta from "vue-meta";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import dateFilter from "@/filters/date.filter";
+import currencyFilter from "@/filters/currency.filter";
+import localizeFilter from "@/filters/localize.filter";
+import tooltipDirective from "@/directives/tooltip.directive";
+import messagePlugin from "@/utils/message.plugin";
+import titlePlugin from "@/utils/title.plugin";
+import Loader from "@/components/app/Loader";
+import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min.js";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+Vue.use(messagePlugin);
+Vue.use(Vuelidate);
+Vue.use(VueMeta);
+Vue.use(titlePlugin);
+Vue.filter("date", dateFilter);
+Vue.filter("currency", currencyFilter);
+Vue.filter("localize", localizeFilter);
+Vue.directive("tooltip", tooltipDirective);
+Vue.component("Loader", Loader);
+Vue.component("Paginate", Paginate);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCiYORX60Uv1kc6eRnOFI-LjuGZfWd3c1Q",
+  authDomain: "vue-crm-12a20.firebaseapp.com",
+  databaseURL: "https://vue-crm-12a20.firebaseio.com",
+  projectId: "vue-crm-12a20",
+  storageBucket: "vue-crm-12a20.appspot.com",
+  messagingSenderId: "188468920091",
+  appId: "1:188468920091:web:86fb63b7c885d976945dbe",
+  measurementId: "G-VC2X9TWL9M"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
